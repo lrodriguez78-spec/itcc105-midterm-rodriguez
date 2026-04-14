1ae9ea88e69e6df398f3d7fc56190063bb493e60
Leah Jean Rodriguez <leahjeanrodriguez4@gmail.com>
2026-04-13 23:48:41 -0700

    Update Dart code

--- copilot_engine.dart
+++ copilot_engine.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
 
 void main() {
-  String userIntent = "check clearance";
+  print("Enter your request:");
+  String userIntent = stdin.readLineSync()!;
   bool isInternetConnected = true;
 
   if (!isInternetConnected) {
-    print("No internet. Please connect to continue.");
+    print("No internet connection.");
   } else {
     if (userIntent == "check clearance") {
       print("Your clearance is complete.");
     } else if (userIntent == "view grades") {
       print("Your grades are: 1.5, 1.75, 2.0");
     } else if (userIntent == "enroll subjects") {
-      print("You are now enrolled successfully.");
+      print("You are now enrolled.");
     } else {
       print("Sorry, I don't understand.");
     }
